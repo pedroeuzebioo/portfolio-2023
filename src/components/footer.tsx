d709a27886a56dcd370cb0e9c5e519b06a54be89
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2024-01-05 19:30:00 -0300

    feat: update footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="mt-auto w-full text-center bg-accent px-8 py-4 text-center text-[0.625rem] opacity-75">
-      © 2023 Copyright <span className="font-semibold">Pedro Henrique</span>
+    <footer className="w-full bg-accent py-4 text-center text-[0.625rem] opacity-75">
+      © 2023 Copyright <span className="font-semibold">Pedro Euzebio</span>
     </footer>
   );
 };
