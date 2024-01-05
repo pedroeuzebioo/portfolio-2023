24bb7f9eec4ecf3e9c68e79fc0293c518ef76c1d
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2024-01-05 19:17:48 -0300

    feat(home): update introduction section and contact links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const IntroductionSection = () => {
+const Introduction = () => {
   return (
     <div className="text-center md:text-left">
-      <h1 className="text-3xl font-medium md:text-4xl">Olá, eu sou o</h1>
-      <span className="text-3xl font-semibold text-indigo-500 md:text-4xl">
-        Pedro Henrique
+      <h1 className="text-2xl font-medium">Olá, eu sou o</h1>
+      <span className="text-2xl font-semibold text-indigo-500">
+        Pedro Euzebio
       </span>
-      <p className="text-lg font-medium text-muted-foreground md:text-xl">
+      <p className="font-medium text-muted-foreground">
         Seja bem-vindo ao meu portfólio
       </p>
       <ContactLinks />
@@ -18,17 +18,17 @@ const IntroductionSection = () => {
 
 const ContactLinks = () => {
   return (
-    <div className="mt-2 flex flex-wrap justify-center gap-2 md:justify-start">
+    <div className="mt-4 flex flex-wrap justify-center gap-3 md:justify-start">
       <a
         href="/curriculo.pdf"
         download
-        className="flex w-full max-w-xs items-center justify-center rounded-full bg-indigo-500 px-4 py-2 font-medium md:px-8"
+        className="flex w-full max-w-[18.75rem] items-center justify-center rounded-full bg-indigo-500 p-2 font-medium"
       >
         Download CV
       </a>
       <Link
         href="mailto:eupedrohenrique.euzebio@gmail.com"
-        className="flex w-full max-w-xs items-center justify-center rounded-full border-2 border-indigo-500 bg-background px-4 py-2 font-medium transition-all hover:bg-indigo-500 md:px-8"
+        className="flex w-full max-w-[18.75rem] items-center justify-center rounded-full border-2 border-indigo-500 bg-background px-1 py-2 font-medium transition-all hover:bg-indigo-500"
       >
         Entrar em contato
       </Link>
@@ -38,17 +38,15 @@ const ContactLinks = () => {
 
 export default function Home() {
   return (
-    <main className="p-5">
+    <main className="m-auto w-full max-w-7xl flex-1 p-[1.875rem]">
       <section className="flex flex-col items-center justify-center gap-4 md:flex-row md:justify-between">
-        <IntroductionSection />
-        <div>
-          <Image
-            src="/avatar.png"
-            alt="Foto do Pedro Henrique"
-            width={200}
-            height={200}
-          />
-        </div>
+        <Introduction />
+        <Image
+          src="/avatar.png"
+          alt="Foto do Pedro Henrique"
+          width={200}
+          height={200}
+        />
       </section>
     </main>
   );
