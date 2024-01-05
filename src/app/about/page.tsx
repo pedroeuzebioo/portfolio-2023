a67a6c2212f96bc457cccacb6ba80fbd613b772c
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2024-01-05 19:19:50 -0300

    feat(about): update About Me section content and styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,41 +1,19 @@
-import Image from "next/image";
-
-export default function AboutMeSection() {
+export default function AboutMeRoute() {
   return (
-    <section className="mb-5 p-5">
-      <h2 className="mb-4 text-center text-3xl font-medium md:text-4xl">
-        Sobre mim
-      </h2>
+    <main className="max-w-7xl flex-1 p-[1.875rem] m-auto">
+      <section className="flex flex-col items-center gap-4">
+        <h2 className="text-2xl font-medium">Sobre mim</h2>
 
-      <div className="flex flex-col gap-3 md:flex-row">
-        <Image
-          src="/pedro.jpg"
-          alt="Foto do Pedro Henrique"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto w-full grayscale md:max-h-[350px]"
-        />
-
-        <p className="text-justify text-lg font-medium text-muted-foreground md:text-xl">
-          Minha jornada começou durante o ensino médio técnico
-          profissionalizante no curso de Informática. Desde então, meu interesse
-          e minha paixão por programação web cresceram no decorrer do curso.{" "}
-          <br /> <br />
-          Sou um entusiasta do desenvolvimento web. Minha primeira experiência
-          profissional foi como Desenvolvedor Web no IFCE Campus Baturité, onde
-          comecei a explorar o mundo da programação. Atualmente, atuo como
-          Técnico de TI no FMAS de Baturité, onde estou envolvido em várias
-          atividades relacionadas à tecnologia. <br></br> <br />
-          Além do meu trabalho, tenho uma paixão por ensinar e ajudar os outros.
-          Tenho o objetivo de me tornar um educador na área da programação,
-          compartilhando meu conhecimento e ajudando outros a adquirir
-          habilidades valiosas e seu potencial máximo. Estou sempre aberto a
-          novos desafios e oportunidades de aprendizado, e estou animado para
-          continuar minha jornada, com um foco em contribuir para a comunidade
-          de tecnologia e fazer a diferença na vida das pessoas.
+        <p className="text-justify text-muted-foreground">
+          Desenvolvedor web e graduado como técnico em informática.
+          Especializado em React, Next.js, Tailwind CSS e Prisma para
+          desenvolvimento fullstack. Experiência sólida no IFCE Campus Baturité,
+          atuando em projetos notáveis. Atuei como Técnico de TI no FMAS de
+          Baturité, continuo aprimorando minhas habilidades e compartilhando
+          conhecimento. Busco oportunidades em projetos inovadores e
+          colaborativos.
         </p>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 }
