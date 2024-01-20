bafc8a8d2fa6fd6347b48e33b753e71f619381de
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-01-20 12:59:37 -0300

    refactor(footer): center-align text in footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+    <footer className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm text-center">
       © 2023 Copyright <span className="font-semibold">Pedro Euzebio</span>
     </footer>
   );
