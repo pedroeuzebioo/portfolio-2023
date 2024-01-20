56ac0dd0bbbfb7d6e9d2bf833c4377848a63f721
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-01-20 12:56:15 -0300

    refactor(about): improve about section in about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,30 +1,19 @@
-"use client";
-
-import "aos/dist/aos.css";
-import { useEffect } from "react";
-
 const About = () => {
-  useEffect(() => {
-    import("aos").then((AOS) => {
-      AOS.default.init();
-    });
-  }, []);
-
   return (
     <section
-      className="m-auto mb-[9.375rem] flex max-w-5xl flex-col items-center gap-4"
-      data-aos="fade-right"
-      data-aos-duration="1000"
-      data-aos-easing="ease-in-out"
+      className="m-auto max-w-3xl py-[6.25rem] appear"
+      id="about"
     >
-      <h2 className="text-2xl font-medium" id="about">
+      <h2 className="py-[0.9375rem] text-center text-2xl font-medium">
         Sobre mim
       </h2>
 
       <p className="text-justify text-muted-foreground">
         Sou formado em Informática pela EEEP, com expertise em React, Next.js,
         Tailwind CSS, Node.js e SQL, utilizando Prisma para desenvolvimento
-        fullstack. Experiência consolidada no IFCE Campus Baturité, atuando como
+        fullstack.
+        <br /> <br />
+        Experiência consolidada no IFCE Campus Baturité, atuando como
         desenvolvedor e solucionador de problemas. Anteriormente, fui Técnico de
         TI no FMAS de Baturité. Estou constantemente aprimorando minhas
         habilidades e compartilhando conhecimento. Busco oportunidades em
