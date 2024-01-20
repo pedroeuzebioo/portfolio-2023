98960136dabb88b3f4d8145ce8e9dcb74da08898
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-01-20 12:47:42 -0300

    refactor(header): update mobile menu in header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,93 +1,22 @@
 "use client";
 
+import Link from "next/link";
+import { Card } from "./ui/card";
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navegation-menu";
-import { navigationMenuTriggerStyle } from "@/components/ui/navegation-menu";
-import Link from "next/link";
-import { Card } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTrigger,
-} from "./ui/sheet";
 
 const Header = () => {
   return (
-    <Card className="relative top-0 z-50 w-full">
-      <div className="m-auto flex max-w-screen-xl items-center justify-between p-[1.875rem]">
-        <Link href="/">
+    <Card className="fixed z-10 w-full p-[1.875rem]" id="home">
+      <div className="flex max-w-screen-xl items-center justify-center md:justify-between">
+        <Link href="#home">
           <h1 className="text-3xl font-semibold">Pedro Euzebio</h1>
         </Link>
-
-        <div className="flex md:hidden">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button size="icon" variant="outline">
-                <MenuIcon />
-              </Button>
-            </SheetTrigger>
-
-            <SheetContent side="right" className="w-[21.875rem]">
-              <SheetHeader className="text-left text-lg font-semibold">
-                Menu
-              </SheetHeader>
-
-              <div className="mt-4 flex flex-col gap-2">
-                <SheetClose asChild>
-                  <Link href="#about">
-                    <Button
-                      variant="outline"
-                      className="w-full justify-start gap-2"
-                    >
-                      Sobre mim
-                    </Button>
-                  </Link>
-                </SheetClose>
-
-                <SheetClose asChild>
-                  <Link href="#technologies">
-                    <Button
-                      variant="outline"
-                      className="w-full justify-start gap-2"
-                    >
-                      Tecnologias
-                    </Button>
-                  </Link>
-                </SheetClose>
-
-                <SheetClose asChild>
-                  <Link href="#projects">
-                    <Button
-                      variant="outline"
-                      className="w-full justify-start gap-2"
-                    >
-                      Projetos
-                    </Button>
-                  </Link>
-                </SheetClose>
-
-                <SheetClose asChild>
-                  <Link href="#contact">
-                    <Button
-                      variant="outline"
-                      className="w-full justify-start gap-2"
-                    >
-                      Contato
-                    </Button>
-                  </Link>
-                </SheetClose>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </div>
 
         <div className="hidden md:flex">
           <NavigationMenu>
