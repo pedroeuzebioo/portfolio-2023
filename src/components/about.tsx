6e569802bb7fb58b22721b4b40378dd9d30cee65
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-01-24 06:31:27 -0300

    feat: update resume about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,23 +1,18 @@
 const About = () => {
   return (
-    <section
-      className="m-auto max-w-3xl py-[6.25rem] appear"
-      id="about"
-    >
+    <section className="appear m-auto max-w-3xl py-[6.25rem]" id="about">
       <h2 className="py-[0.9375rem] text-center text-2xl font-medium">
         Sobre mim
       </h2>
 
       <p className="text-justify text-muted-foreground">
-        Sou formado em Informática pela EEEP, com expertise em React, Next.js,
-        Tailwind CSS, Node.js e SQL, utilizando Prisma para desenvolvimento
-        fullstack.
+        Formado em Técnico em Informática, atualmente cursando Informática para
+        Internet no IFCE Campus Baturité.
         <br /> <br />
-        Experiência consolidada no IFCE Campus Baturité, atuando como
-        desenvolvedor e solucionador de problemas. Anteriormente, fui Técnico de
-        TI no FMAS de Baturité. Estou constantemente aprimorando minhas
-        habilidades e compartilhando conhecimento. Busco oportunidades em
-        projetos colaborativos.
+        Com experiência no IFCE Campus Baturité e FMAS de Baturité, destaco
+        minha atuação como desenvolvedor web, otimizando sistemas e garantindo a
+        segurança de dados. Comprometido com resultados, busco constantemente
+        aprimorar minhas habilidades para contribuir com excelência.
       </p>
     </section>
   );
